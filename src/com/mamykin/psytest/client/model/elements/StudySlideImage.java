b86ae6d9b0f9a9cad824aa85efa11e949797e23c
Kliment Mamykin <kmamyk@yahoo.com>
2009-08-01 22:43:31 -0400

    Added StudySlideImage class to represent a picture on a study slide. Added reading an xml in studyServiceimpl.

---
+++ src/com/mamykin/psytest/client/model/elements/StudySlideImage.java
@@ -0,0 +1,41 @@
+package com.mamykin.psytest.client.model.elements;
+
+import java.io.Serializable;
+
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
+import com.mamykin.psytest.client.model.StudyResultLogger;
+import com.mamykin.psytest.client.model.StudySlideElement;
+
+public class StudySlideImage extends StudySlideElement implements Serializable {
+
+	private static final long serialVersionUID = -327405498439968945L;
+	private String imageRelativeUrl;
+
+	public StudySlideImage() {
+		this("", "");
+	}
+
+	public StudySlideImage(String id, String imageRelativeUrl) {
+		super(id);
+		this.imageRelativeUrl = imageRelativeUrl;
+	}
+
+	@Override
+	public Widget createUIElement() {
+		Image image = new Image();
+		image.setUrl("http://localhost:8080" + imageRelativeUrl);
+		return image;
+	}
+
+	@Override
+	public boolean isUIValid() {
+		return true;
+	}
+
+	@Override
+	public void recordResult(StudyResultLogger logger) {
+		// do nothing
+	}
+
+}
--- src/com/mamykin/psytest/server/StudyServiceImpl.java
+++ src/com/mamykin/psytest/server/StudyServiceImpl.java
@@ -4,11 +4,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 import com.mamykin.psytest.client.StudyService;
 import com.mamykin.psytest.client.model.Study;
 import com.mamykin.psytest.client.model.StudyRun;
 import com.mamykin.psytest.client.model.StudyRunResults;
 import com.mamykin.psytest.client.model.StudySlide;
+import com.mamykin.psytest.client.model.elements.StudySlideImage;
 import com.mamykin.psytest.client.model.elements.StudySlideParagraph;
 import com.mamykin.psytest.client.model.elements.StudySlideSingleChoice;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -20,7 +29,8 @@ import com.google.appengine.api.mail.*;
 @SuppressWarnings("serial")
 public class StudyServiceImpl extends RemoteServiceServlet implements
 		StudyService {
-	private static final Logger log = Logger.getLogger(StudyServiceImpl.class.getName());
+	private static final Logger log = Logger.getLogger(StudyServiceImpl.class
+			.getName());
 
 	public Study getStudy() {
 		Study newStudy = new Study();
@@ -50,16 +60,20 @@ public class StudyServiceImpl extends RemoteServiceServlet implements
 			+ "qui officia deserunt mollit anim id est laborum.";
 
 	private final String defaultQuestion = "Blah blah blah?";
-	
+
 	public StudyRun getStudyRun(String groupName, String caseName,
 			String participant) {
+		String studyname = getStudyName();
+
 		ArrayList<StudySlide> slides = new ArrayList<StudySlide>();
 
 		StudySlide slide1 = new StudySlide();
 		slide1.addElement(new StudySlideParagraph("1", loremIpsum)).addElement(
 				new StudySlideParagraph("2", loremIpsum)).addElement(
-				new StudySlideSingleChoice("3", defaultQuestion, new String[] {
-						"Option a", "Option b", "Option c" }, 2));
+				new StudySlideParagraph("3", studyname)).addElement(
+				new StudySlideSingleChoice("4", defaultQuestion, new String[] {
+						"Option a", "Option b", "Option c" }, 2)).addElement(
+				new StudySlideImage("5", "/images/breakfast.jpg"));
 		slides.add(slide1);
 
 		StudySlide slide2 = new StudySlide();
@@ -73,9 +87,32 @@ public class StudyServiceImpl extends RemoteServiceServlet implements
 		return new StudyRun(participant, slides);
 	}
 
+	private String getStudyName() {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+
+			// Using factory get an instance of document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			// parse using builder to get DOM representation of the XML file
+			Document dom = db.parse("http://localhost:8080/study.xml");
+			Element docEle = dom.getDocumentElement();
+			return docEle.getAttribute("name");
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (SAXException se) {
+			se.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		return null;
+	}
+
 	public boolean recordRunResults(StudyRunResults results) {
 		MailService mailService = MailServiceFactory.getMailService();
-		MailService.Message email = new MailService.Message("kmamyk@gmail.com", "kmamyk@gmail.com", "Study results", results.formatAsEmailBody());
+		MailService.Message email = new MailService.Message("kmamyk@gmail.com",
+				"kmamyk@gmail.com", "Study results", results
+						.formatAsEmailBody());
 		try {
 			log.info(email.getTextBody());
 			mailService.send(email);
